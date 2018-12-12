27403d1446e4fc0c71d6f111af49dda13e5aff20
Akshay Rajaraman Subramanian <akshayrs@terpmail.umd.edu>
2018-12-12 04:26:43 -0500

    [INTEG] Refactored explore class to include wrapper

--- src/Explore.cpp
+++ src/Explore.cpp
@@ -19,8 +19,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -33,15 +33,17 @@
 
 #include "../include/Explore.h"
 #include <math.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <stack>
+#include <vector>
+
 #include "geometry_msgs/Twist.h"
-#include "sensor_msgs/LaserScan.h"
 #include "nav_msgs/OccupancyGrid.h"
+#include "sensor_msgs/LaserScan.h"
 #include "tf/transform_listener.h"
 
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <stack>
 #define PI 3.141592
 
 /**
@@ -49,14 +51,7 @@
  * @param NodeHandle n
  */
 
-Explore::Explore(ros::NodeHandle &n) : n(n) {
-	//publish motor commands
-	motor_command_publisher = n.advertise<geometry_msgs::Twist> ( "/mobile_base/commands/velocity", 100 );
-	//subscribe to laser data
-    laser_subscriber = n.subscribe ( "/scan", 1000, &Explore::getLaserData, this);
-	//subscribe to map data    
-	map_subscriber = n.subscribe ( "/map", 1000, &Explore::getMapData, this);
-}
+Explore::Explore() {}
 
 /**
  * @brief destructor function
@@ -64,55 +59,38 @@ Explore::Explore(ros::NodeHandle &n) : n(n) {
 
 Explore::~Explore() {}
 
-
-
 /**
  * @brief function to set the movement of the robot
  * @param movement type [LEFT, RIGHT, FOWARD, REVERSE etc]
  */
 
-bool Explore::robot_move ( const ROBOT_MOVEMENT move_type ) {
-	// set motor commands for each scenario    
-	if ( move_type == STOP ) {
-        
-        motor_command.angular.z = 0.0;
-        motor_command.linear.x = 0.0;
-    }
-
-    else if ( move_type == FORWARD ) {
-        motor_command.angular.z = 0.0;
-        motor_command.linear.x = 0.5;
-    }
-
-    else if ( move_type == BACKWARD ) {
-        motor_command.linear.x = -0.75;
-        motor_command.angular.z = 0.0;
-    }
-
-    else if ( move_type == TURN_LEFT ) {
-        motor_command.linear.x = 0.0;
-        motor_command.angular.z = 0.75;
-    }
-
-    else if ( move_type == TURN_RIGHT ) {
-        motor_command.linear.x = 0.0;
-        motor_command.angular.z = -0.75;
-    } 
-    else if ( move_type == GO_RIGHT ) {
-        motor_command.linear.x = 0.25;
-        motor_command.angular.z = -0.25;
-    }
-    else if ( move_type == GO_LEFT ) {
-        motor_command.linear.x = 0.25;
-        motor_command.angular.z = 0.25;
-    }
-    else {
-        return false;
-    }
-	// publish motor command
-    motor_command_publisher.publish ( motor_command );
-    usleep(10);
-    return true;
+bool Explore::robot_move(const ROBOT_MOVEMENT move_type) {
+  // set motor commands for each scenario
+  if (move_type == STOP) {
+    motor_command.angular.z = 0.0;
+    motor_command.linear.x = 0.0;
+  } else if (move_type == FORWARD) {
+    motor_command.angular.z = 0.0;
+    motor_command.linear.x = 0.5;
+  } else if (move_type == BACKWARD) {
+    motor_command.linear.x = -0.5;
+    motor_command.angular.z = 0.0;
+  } else if (move_type == TURN_LEFT) {
+    motor_command.linear.x = 0.0;
+    motor_command.angular.z = 0.5;
+  } else if (move_type == TURN_RIGHT) {
+    motor_command.linear.x = 0.0;
+    motor_command.angular.z = -0.5;
+  } else if (move_type == GO_RIGHT) {
+    motor_command.linear.x = 0.25;
+    motor_command.angular.z = -0.25;
+  } else if (move_type == GO_LEFT) {
+    motor_command.linear.x = 0.25;
+    motor_command.angular.z = 0.25;
+  } else {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -120,90 +98,84 @@ bool Explore::robot_move ( const ROBOT_MOVEMENT move_type ) {
  * @param pointer to laser scan data
  */
 
-void Explore::getLaserData ( const sensor_msgs::LaserScan::ConstPtr &scan_msg ) {
-    laser_msg = *scan_msg;
-    std::vector<float> laser_ranges;
-	// get laser scan range
-    laser_ranges = laser_msg.ranges;
-    size_t range_size = laser_ranges.size();    
-    float left_side = 0.0, right_side = 0.0;
-	// get maximum and minimum ranges for laser scan
-    float range_min = laser_msg.range_max;
-	float range_max = laser_msg.range_min;
-    int nan_count = 0;
-    for(size_t i = 0; i < range_size; i++){
-        if (laser_ranges[i] < range_min){
-            range_min = laser_ranges[i];
-        }
-        
-        if (std::isnan(laser_ranges[i])){
-            nan_count++;
-        }
-        if (i < range_size / 4 ) {
-            if (laser_ranges[i] > range_max){
-                range_max = laser_ranges[i];
-            }
-        }
-        
-        if (i > range_size / 2){
-            left_side += laser_ranges[i];
-        }
-        else {
-            right_side += laser_ranges[i];
-        }
+geometry_msgs::Twist Explore::getLaserData(
+    const sensor_msgs::LaserScan::ConstPtr &scan_msg) {
+  laser_msg = *scan_msg;
+  std::vector<float> laser_ranges;
+  // get laser scan range
+  laser_ranges = laser_msg.ranges;
+  size_t range_size = laser_ranges.size();
+  float left_side = 0.0, right_side = 0.0;
+  // get maximum and minimum ranges for laser scan
+  float range_min = laser_msg.range_max;
+  float range_max = laser_msg.range_min;
+  int nan_count = 0;
+  for (size_t i = 0; i < range_size; i++) {
+    if (laser_ranges[i] < range_min) {
+      range_min = laser_ranges[i];
     }
-    
-    if (nan_count > (range_size * 0.9) || laser_ranges[range_size / 2] < 0.25) {
-        crashed = true;
+
+    if (std::isnan(laser_ranges[i])) {
+      nan_count++;
     }
-    else {
-        crashed = false;
+    if (i < range_size / 4) {
+      if (laser_ranges[i] > range_max) {
+        range_max = laser_ranges[i];
+      }
     }
-    if (!crashed) {
-        
-        if (range_min <= 1 && !thats_a_door){
-            following_wall = true;
-            crashed = false;
-			// stop the robot before taking a decision to move
-            robot_move(STOP);
-            
-            if (left_side >= right_side) {
-				// turn the robot to the right
-                robot_move(TURN_RIGHT);
-            }
-            else {
-				// turn the robot to the left
-                robot_move(TURN_LEFT);  
-            }
+
+    if (i > range_size / 2) {
+      left_side += laser_ranges[i];
+    } else {
+      right_side += laser_ranges[i];
+    }
+  }
+
+  if (nan_count > (range_size * 0.9) || laser_ranges[range_size / 2] < 0.25) {
+    crashed = true;
+  } else {
+    crashed = false;
+  }
+  if (!crashed) {
+    if (range_min <= 1 && !thats_a_door) {
+      following_wall = true;
+      crashed = false;
+      // stop the robot before taking a decision to move
+      robot_move(STOP);
+
+      if (left_side >= right_side) {
+        // turn the robot to the right
+        robot_move(TURN_RIGHT);
+      } else {
+        // turn the robot to the left
+        robot_move(TURN_LEFT);
+      }
+    } else {
+      robot_move(STOP);
+      if (following_wall) {
+        if (range_max >= 1.5) {
+          thats_a_door = true;
+          following_wall = false;
         }
-        else {
-            robot_move(STOP);
-            if ( following_wall ) {
-                if (range_max >= 1.5){
-                    thats_a_door = true;
-                    following_wall = false;
-                }
-            } 
-            if (thats_a_door) {
-                if (laser_ranges[0] <= 1){
-                    thats_a_door = false;
-                }
-                else {
-					// move the robot to the right
-                    robot_move(GO_RIGHT);  
-                }
-                
-            }
-            else {
-				// move the robot forward
-                robot_move(FORWARD);  
-            }
+      }
+      if (thats_a_door) {
+        if (laser_ranges[0] <= 1) {
+          thats_a_door = false;
+        } else {
+          // move the robot to the right
+          robot_move(GO_RIGHT);
         }
+
+      } else {
+        // move the robot forward
+        robot_move(FORWARD);
+      }
     }
-    else {
-		// move the robot backwards
-        robot_move(BACKWARD);
-    }
+  } else {
+    // move the robot backwards
+    robot_move(BACKWARD);
+  }
+  return motor_command;
 }
 
 /**
@@ -211,14 +183,13 @@ void Explore::getLaserData ( const sensor_msgs::LaserScan::ConstPtr &scan_msg ) 
  * @param pointer to occupancy grid data
  */
 
-void Explore::getMapData ( const nav_msgs::OccupancyGrid::ConstPtr &msg ) {
-    const bool chatty_map = true;
-    map_msg = *msg;
-    double map_width = map_msg.info.width;
-    double map_height = map_msg.info.width;
-    double map_origin_x = map_msg.info.origin.position.x;
-    double map_origin_y = map_msg.info.origin.position.y;
-    double map_orientation = acos ( map_msg.info.origin.orientation.z );
-    std::vector<signed char > map = map_msg.data;
+void Explore::getMapData(const nav_msgs::OccupancyGrid::ConstPtr &msg) {
+  const bool chatty_map = true;
+  map_msg = *msg;
+  double map_width = map_msg.info.width;
+  double map_height = map_msg.info.width;
+  double map_origin_x = map_msg.info.origin.position.x;
+  double map_origin_y = map_msg.info.origin.position.y;
+  double map_orientation = acos(map_msg.info.origin.orientation.z);
+  std::vector<signed char> map = map_msg.data;
 }
-
